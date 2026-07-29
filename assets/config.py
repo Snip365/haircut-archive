e19e804ba2629119108c80073f10ce519a28a7d9
haxxer365 <sirtayke@gmail.com>
2026-07-28 21:12:03 -0300

    Filter function

---
+++ assets/config.py
@@ -0,0 +1,22 @@
+MEDIA_TAGS = [
+    "Manga",
+    "Anime",
+    "Light Novel",
+    "Manhwa",
+    "Manhua",
+    "Onscreen"
+]
+
+STYLE_TAGS = [
+    "Long",
+    "Bob",
+    "Pixie",
+    "Bald",
+    "Dye",
+    "Braid",
+    "Twintail",
+    "Ponytail",
+    "Himecut",
+    "Wolfcut",
+    "Other"
+]
